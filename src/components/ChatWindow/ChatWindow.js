90c21cc0349cbd2b3997a5c3119962b2a042021f
maxatthemax <b9016183@newcastle.ac.uk>
2020-02-26 17:20:30 +0000

    bottom message comes onto screen

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import TextMessageBox from '../TextMessageBox'
 
 function ChatWindow({ messages, user }) {
+
+  let scroll = useRef(null);
+
+  useEffect(() => {
+
+    scroll.current.scrollIntoView({ behaviour: "smooth"})
+
+  }, [messages])
 
   return (
     <div className={`chatWindow`}>
       {messages && messages.map((message) => {
         return <TextMessageBox message={message} key={message.id} user={user} />;
       })}
+      <div ref={scroll} />
     </div>
   );
 }
